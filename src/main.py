4e18d80ffabfc2226aae314e3f780c9df2e37f6e
Michal Kváček <michal@kvacek.cz>
2021-06-09 09:45:57 +0200

    Update GAM API version to v202105

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from config import Config
 
 # Deprecation = May 2021
 # Sunset = August 2021
-API_VERSION = "v202008"
+API_VERSION = "v202105"
 
 APPLICATION_NAME = "performax.ex-google-ad-manager-report-service"
 OUTPUT_FILE = '/data/out/tables/output.csv'
